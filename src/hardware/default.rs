68d2155e670ebac45f7b1b25539e7520308d7e45
odashi <yus.takara@gmail.com>
2022-01-25 18:27:51 -0800

    add test for get_default_device

--- src/hardware/default.rs
+++ src/hardware/default.rs
@@ -11,3 +11,18 @@ pub(crate) fn get_default_hardware() -> &'static Mutex<Box<dyn Hardware>> {
     static SINGLETON: OnceCell<Mutex<Box<dyn Hardware>>> = OnceCell::new();
     SINGLETON.get_or_init(|| Mutex::new(Box::new(Cpu::new("default"))))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::hardware::get_default_hardware;
+
+    #[test]
+    fn test_get_default_device() {
+        let dev1 = get_default_hardware();
+        assert_eq!(dev1.lock().unwrap().name(), "default");
+
+        // Default device must be a singleton.
+        let dev2 = get_default_hardware();
+        assert!(std::ptr::eq(dev2, dev2));
+    }
+}
