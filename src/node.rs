156f45b8d3d977d4b446a2cf8ba63a6006c38a09
odashi <yus.takara@gmail.com>
2022-02-23 12:00:30 +0000

    add tests.

--- src/node.rs
+++ src/node.rs
@@ -256,6 +256,7 @@ mod tests {
     use crate::make_shape;
     use crate::node::Node;
     use std::cell::RefCell;
+    use std::ptr;
 
     #[test]
     fn test_steps() {
@@ -265,9 +266,15 @@ mod tests {
         let rhs = Node::from_scalar(&hw, &g, 2.);
         let ret = lhs + rhs;
 
+        assert_eq!(lhs, Node::new(&g, 0));
+        assert_eq!(rhs, Node::new(&g, 1));
+        assert_eq!(ret, Node::new(&g, 2));
         assert_eq!(lhs.shape(), make_shape![]);
         assert_eq!(rhs.shape(), make_shape![]);
         assert_eq!(ret.shape(), make_shape![]);
+        assert!(ptr::eq(lhs.hardware(), &hw));
+        assert!(ptr::eq(rhs.hardware(), &hw));
+        assert!(ptr::eq(ret.hardware(), &hw));
 
         {
             let g = g.borrow();
@@ -276,10 +283,6 @@ mod tests {
             assert_eq!(g.get_step(1).unwrap().operator.name(), "Constant");
             assert_eq!(g.get_step(2).unwrap().operator.name(), "Add");
         }
-
-        assert_eq!(lhs, Node::new(&g, 0));
-        assert_eq!(rhs, Node::new(&g, 1));
-        assert_eq!(ret, Node::new(&g, 2));
 
         let retval = ret.calculate().unwrap();
         assert_eq!(*retval.shape(), make_shape![]);
@@ -296,6 +299,8 @@ mod tests {
 
         assert_eq!(src.shape(), make_shape![]);
         assert_eq!(dest.shape(), make_shape![]);
+        assert!(ptr::eq(src.hardware(), &hw));
+        assert!(ptr::eq(dest.hardware(), &hw));
 
         assert_eq!(dest.calculate().unwrap().get_scalar_f32(), Ok(-42.));
     }
@@ -312,6 +317,9 @@ mod tests {
         assert_eq!(lhs.shape(), make_shape![]);
         assert_eq!(rhs.shape(), make_shape![]);
         assert_eq!(ret.shape(), make_shape![]);
+        assert!(ptr::eq(lhs.hardware(), &hw));
+        assert!(ptr::eq(rhs.hardware(), &hw));
+        assert!(ptr::eq(ret.hardware(), &hw));
 
         assert_eq!(ret.calculate().unwrap().get_scalar_f32(), Ok(3.));
     }
@@ -328,6 +336,9 @@ mod tests {
         assert_eq!(lhs.shape(), make_shape![]);
         assert_eq!(rhs.shape(), make_shape![]);
         assert_eq!(ret.shape(), make_shape![]);
+        assert!(ptr::eq(lhs.hardware(), &hw));
+        assert!(ptr::eq(rhs.hardware(), &hw));
+        assert!(ptr::eq(ret.hardware(), &hw));
 
         assert_eq!(ret.calculate().unwrap().get_scalar_f32(), Ok(-1.));
     }
@@ -344,6 +355,9 @@ mod tests {
         assert_eq!(lhs.shape(), make_shape![]);
         assert_eq!(rhs.shape(), make_shape![]);
         assert_eq!(ret.shape(), make_shape![]);
+        assert!(ptr::eq(lhs.hardware(), &hw));
+        assert!(ptr::eq(rhs.hardware(), &hw));
+        assert!(ptr::eq(ret.hardware(), &hw));
 
         assert_eq!(ret.calculate().unwrap().get_scalar_f32(), Ok(2.));
     }
@@ -360,6 +374,9 @@ mod tests {
         assert_eq!(lhs.shape(), make_shape![]);
         assert_eq!(rhs.shape(), make_shape![]);
         assert_eq!(ret.shape(), make_shape![]);
+        assert!(ptr::eq(lhs.hardware(), &hw));
+        assert!(ptr::eq(rhs.hardware(), &hw));
+        assert!(ptr::eq(ret.hardware(), &hw));
 
         assert_eq!(ret.calculate().unwrap().get_scalar_f32(), Ok(0.5));
     }
@@ -370,6 +387,7 @@ mod tests {
         let g = RefCell::new(Graph::new());
         let ret = Node::fill(&hw, &g, make_shape![], 123.);
         assert_eq!(ret.shape(), make_shape![]);
+        assert!(ptr::eq(ret.hardware(), &hw));
         assert_eq!(ret.calculate().unwrap().get_scalar_f32(), Ok(123.));
     }
 
@@ -379,6 +397,7 @@ mod tests {
         let g = RefCell::new(Graph::new());
         let ret = Node::fill(&hw, &g, make_shape![0], 123.);
         assert_eq!(ret.shape(), make_shape![0]);
+        assert!(ptr::eq(ret.hardware(), &hw));
         assert_eq!(ret.calculate().unwrap().get_values_f32(), vec![]);
     }
 
@@ -388,6 +407,7 @@ mod tests {
         let g = RefCell::new(Graph::new());
         let ret = Node::fill(&hw, &g, make_shape![3], 123.);
         assert_eq!(ret.shape(), make_shape![3]);
+        assert!(ptr::eq(ret.hardware(), &hw));
         assert_eq!(
             ret.calculate().unwrap().get_values_f32(),
             vec![123., 123., 123.]
@@ -408,6 +428,10 @@ mod tests {
         assert_eq!(b.shape(), make_shape![]);
         assert_eq!(c.shape(), make_shape![]);
         assert_eq!(y.shape(), make_shape![]);
+        assert!(ptr::eq(a.hardware(), &hw));
+        assert!(ptr::eq(b.hardware(), &hw));
+        assert!(ptr::eq(c.hardware(), &hw));
+        assert!(ptr::eq(y.hardware(), &hw));
 
         assert_eq!(y.calculate().unwrap().get_scalar_f32(), Ok(-5.));
     }
