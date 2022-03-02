f0d535eb0c3369e9defca1f6c12e93429aae40d1
odashi <yus.takara@gmail.com>
2022-03-02 20:43:23 +0000

    implements TryFrom<Array> for Array0 and add ndarray-support feature.

--- src/array.rs
+++ src/array.rs
@@ -410,12 +410,8 @@ impl<'hw> Clone for Array<'hw> {
     }
 }
 
-impl<'hw> TryFrom<&Array<'hw>> for ndarray::Array0<f32> {
-    type Error = crate::error::Error;
-    fn try_from(_src: &Array<'hw>) -> Result<Self> {
-        panic!("not implemented");
-    }
-}
-
 #[cfg(test)]
 mod tests;
+
+#[cfg(feature = "ndarray-support")]
+mod convert_ndarray;
