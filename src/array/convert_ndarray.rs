3bf29bd18c730994544c62cf47b302220cbfc6e8
odashi <yus.takara@gmail.com>
2022-03-03 04:21:56 +0000

    TryFrom<Array> for ndarray::Array1

--- src/array/convert_ndarray.rs
+++ src/array/convert_ndarray.rs
@@ -8,5 +8,19 @@ impl<'hw> TryFrom<&Array<'hw>> for ndarray::Array0<f32> {
     }
 }
 
+impl<'hw> TryFrom<&Array<'hw>> for ndarray::Array1<f32> {
+    type Error = Error;
+    fn try_from(src: &Array<'hw>) -> Result<Self> {
+        if let Ok(shape) = src.shape.as_array::<1>() {
+            Ok(unsafe { Self::from_shape_vec_unchecked(shape, src.get_values_f32()) })
+        } else {
+            Err(Error::InvalidShape(format!(
+                "Attempted to generate ndarray::Array1 from {}-dimensional Array.",
+                src.shape().num_dimensions()
+            )))
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests;
--- src/array/convert_ndarray/tests.rs
+++ src/array/convert_ndarray/tests.rs
@@ -15,3 +15,28 @@ fn test_try_into_array0_fail() {
     let src = Array::fill_f32(&hw, Shape::new([1]), 42.);
     assert!(ndarray::Array0::<f32>::try_from(&src).is_err());
 }
+
+#[test]
+fn test_try_into_array1_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0]), 42.);
+    let dest = ndarray::Array1::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr1(&[]));
+}
+
+#[test]
+fn test_try_into_array1_n() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([3]), 42.);
+    let dest = ndarray::Array1::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr1(&[42., 42., 42.]));
+}
+
+#[test]
+fn test_try_into_array1_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src1 = Array::fill_f32(&hw, Shape::new([]), 42.);
+    let src2 = Array::fill_f32(&hw, Shape::new([1, 1]), 42.);
+    assert!(ndarray::Array1::<f32>::try_from(&src1).is_err());
+    assert!(ndarray::Array1::<f32>::try_from(&src2).is_err());
+}
