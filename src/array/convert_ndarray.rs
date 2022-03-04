ede9e61c1de18bca1b44879c0e42e2b8cc3d82d0
Yusuke Oda <yus.takara@gmail.com>
2022-03-05 02:09:26 +0900

    Implements `TryFrom<Array> for ndarray::ArrayN` (#24)
    * swap arguments graph and hardware.
    
    * impl From<Node> for f32
    
    * Update src/node.rs
    
    Co-authored-by: Tetsuo Kiso <tetsuok@users.noreply.github.com>
    
    * from to tryfrom
    
    * add placeholder for ndarray
    
    * implements TryFrom<Array> for Array0 and add ndarray-support feature.
    
    * newline
    
    * add Shape::as_array()
    
    * fix document
    
    * TryFrom<Array> for ndarray::Array1
    
    * impl TryFrom<Array> for ndarray::Array0 to 6
    
    * define as_arrayN separately.
    
    * fix as_arrayN
    
    * fix merge bug
    
    Co-authored-by: Tetsuo Kiso <tetsuok@users.noreply.github.com>

--- src/array/convert_ndarray.rs
+++ src/array/convert_ndarray.rs
@@ -1,12 +1,27 @@
 use crate::array::*;
 
-impl<'hw> TryFrom<&Array<'hw>> for ndarray::Array0<f32> {
-    type Error = Error;
-    fn try_from(src: &Array<'hw>) -> Result<Self> {
-        src.shape().check_is_scalar()?;
-        Ok(unsafe { Self::from_shape_vec_unchecked((), src.get_values_f32()) })
-    }
+// ndarray has separate definitions of Array0 - Array6 although they are based on some generics.
+// We also provide separate definitions of TryFrom<&Array> for each ArrayN to fit the same manner.
+
+macro_rules! define_try_from_array {
+    ( $as_array_fn:ident, $dest:ty ) => {
+        impl<'hw> TryFrom<&Array<'hw>> for $dest {
+            type Error = Error;
+            fn try_from(src: &Array<'hw>) -> Result<Self> {
+                let shape = src.shape.$as_array_fn()?;
+                Ok(unsafe { Self::from_shape_vec_unchecked(shape, src.get_values_f32()) })
+            }
+        }
+    };
 }
+
+define_try_from_array!(as_array0, ndarray::Array0<f32>);
+define_try_from_array!(as_array1, ndarray::Array1<f32>);
+define_try_from_array!(as_array2, ndarray::Array2<f32>);
+define_try_from_array!(as_array3, ndarray::Array3<f32>);
+define_try_from_array!(as_array4, ndarray::Array4<f32>);
+define_try_from_array!(as_array5, ndarray::Array5<f32>);
+define_try_from_array!(as_array6, ndarray::Array6<f32>);
 
 #[cfg(test)]
 mod tests;
--- src/array/convert_ndarray/tests.rs
+++ src/array/convert_ndarray/tests.rs
@@ -12,6 +12,181 @@ fn test_try_into_array0() {
 #[test]
 fn test_try_into_array0_fail() {
     let hw = RefCell::new(CpuHardware::new());
-    let src = Array::fill_f32(&hw, Shape::new([1]), 42.);
-    assert!(ndarray::Array0::<f32>::try_from(&src).is_err());
+    for n in [1, 2, 3, 4, 5, 6, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array0::<f32>::try_from(&src).is_err());
+    }
+}
+
+#[test]
+fn test_try_into_array1_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0]), 42.);
+    let dest = ndarray::Array1::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr1(&[]));
+}
+
+#[test]
+fn test_try_into_array1_3() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([3]), 42.);
+    let dest = ndarray::Array1::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr1(&[42.; 3]));
+}
+
+#[test]
+fn test_try_into_array1_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    for n in [0, 2, 3, 4, 5, 6, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array1::<f32>::try_from(&src).is_err());
+    }
+}
+
+#[test]
+fn test_try_into_array2_0x0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0, 0]), 42.);
+    let dest = ndarray::Array2::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr2(&[[42.; 0]; 0]));
+}
+
+#[test]
+fn test_try_into_array2_2x3() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([2, 3]), 42.);
+    let dest = ndarray::Array2::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr2(&[[42.; 3]; 2]));
+}
+
+#[test]
+fn test_try_into_array2_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    for n in [0, 1, 3, 4, 5, 6, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array2::<f32>::try_from(&src).is_err());
+    }
+}
+
+#[test]
+fn test_try_into_array3_0x0x0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0, 0, 0]), 42.);
+    let dest = ndarray::Array3::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr3(&[[[42.; 0]; 0]; 0]));
+}
+
+#[test]
+fn test_try_into_array3_2x3x4() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([2, 3, 4]), 42.);
+    let dest = ndarray::Array3::<f32>::try_from(&src).unwrap();
+    assert_eq!(dest, ndarray::arr3(&[[[42.; 4]; 3]; 2]));
+}
+
+#[test]
+fn test_try_into_array3_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    for n in [0, 1, 2, 4, 5, 6, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array3::<f32>::try_from(&src).is_err());
+    }
+}
+
+#[test]
+fn test_try_into_array4_0x0x0x0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0, 0, 0, 0]), 42.);
+    let dest = ndarray::Array4::<f32>::try_from(&src).unwrap();
+    assert_eq!(
+        dest,
+        ndarray::Array4::<f32>::from_shape_vec((0, 0, 0, 0), vec![]).unwrap()
+    );
+}
+
+#[test]
+fn test_try_into_array4_2x3x4x5() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([2, 3, 4, 5]), 42.);
+    let dest = ndarray::Array4::<f32>::try_from(&src).unwrap();
+    assert_eq!(
+        dest,
+        ndarray::Array4::<f32>::from_shape_vec((2, 3, 4, 5), vec![42.; 2 * 3 * 4 * 5]).unwrap()
+    );
+}
+
+#[test]
+fn test_try_into_array4_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    for n in [0, 1, 2, 3, 5, 6, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array4::<f32>::try_from(&src).is_err());
+    }
+}
+
+#[test]
+fn test_try_into_array5_0x0x0x0x0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0, 0, 0, 0, 0]), 42.);
+    let dest = ndarray::Array5::<f32>::try_from(&src).unwrap();
+    assert_eq!(
+        dest,
+        ndarray::Array5::<f32>::from_shape_vec((0, 0, 0, 0, 0), vec![]).unwrap()
+    );
+}
+
+#[test]
+fn test_try_into_array5_2x3x4x5x6() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([2, 3, 4, 5, 6]), 42.);
+    let dest = ndarray::Array5::<f32>::try_from(&src).unwrap();
+    assert_eq!(
+        dest,
+        ndarray::Array5::<f32>::from_shape_vec((2, 3, 4, 5, 6), vec![42.; 2 * 3 * 4 * 5 * 6])
+            .unwrap()
+    );
+}
+
+#[test]
+fn test_try_into_array5_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    for n in [0, 1, 2, 3, 4, 6, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array5::<f32>::try_from(&src).is_err());
+    }
+}
+
+#[test]
+fn test_try_into_array6_0x0x0x0x0x0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([0, 0, 0, 0, 0, 0]), 42.);
+    let dest = ndarray::Array6::<f32>::try_from(&src).unwrap();
+    assert_eq!(
+        dest,
+        ndarray::Array6::<f32>::from_shape_vec((0, 0, 0, 0, 0, 0), vec![]).unwrap()
+    );
+}
+
+#[test]
+fn test_try_into_array6_2x3x4x5x6x7() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = Array::fill_f32(&hw, Shape::new([2, 3, 4, 5, 6, 7]), 42.);
+    let dest = ndarray::Array6::<f32>::try_from(&src).unwrap();
+    assert_eq!(
+        dest,
+        ndarray::Array6::<f32>::from_shape_vec(
+            (2, 3, 4, 5, 6, 7),
+            vec![42.; 2 * 3 * 4 * 5 * 6 * 7]
+        )
+        .unwrap()
+    );
+}
+
+#[test]
+fn test_try_into_array6_fail() {
+    let hw = RefCell::new(CpuHardware::new());
+    for n in [0, 1, 2, 3, 4, 5, 7, 8] {
+        let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
+        assert!(ndarray::Array6::<f32>::try_from(&src).is_err());
+    }
 }
