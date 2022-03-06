affc34b5ca4af7cbe9d63565af4cc4260dbb1ce1
odashi <yus.takara@gmail.com>
2022-03-06 09:18:29 +0000

    impl IntoArray for ndarray::ArrayN

--- src/array/convert_ndarray.rs
+++ src/array/convert_ndarray.rs
@@ -3,9 +3,32 @@ use crate::array::*;
 // ndarray has separate definitions of Array0 - Array6 although they are based on some generics.
 // We also provide separate definitions of TryFrom<&Array> for each ArrayN to fit the same manner.
 
+macro_rules! define_into_array {
+    ( $src_ty:ty ) => {
+        impl IntoArray for $src_ty {
+            fn into_array(self, hardware: &RefCell<dyn Hardware>) -> Array {
+                Array::constant_f32(
+                    hardware,
+                    Shape::from_slice(self.shape()),
+                    self.as_standard_layout().as_slice().unwrap(),
+                )
+                .unwrap()
+            }
+        }
+    };
+}
+
+define_into_array!(&ndarray::Array0<f32>);
+define_into_array!(&ndarray::Array1<f32>);
+define_into_array!(&ndarray::Array2<f32>);
+define_into_array!(&ndarray::Array3<f32>);
+define_into_array!(&ndarray::Array4<f32>);
+define_into_array!(&ndarray::Array5<f32>);
+define_into_array!(&ndarray::Array6<f32>);
+
 macro_rules! define_try_from_array {
-    ( $as_array_fn:ident, $dest:ty ) => {
-        impl<'hw> TryFrom<&Array<'hw>> for $dest {
+    ( $as_array_fn:ident, $dest_ty:ty ) => {
+        impl<'hw> TryFrom<&Array<'hw>> for $dest_ty {
             type Error = Error;
             fn try_from(src: &Array<'hw>) -> Result<Self> {
                 let shape = src.shape.$as_array_fn()?;
