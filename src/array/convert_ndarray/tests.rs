6f4ecc0c6b5c7aedc10196b5f83c1919d5597edc
Yusuke Oda <yus.takara@gmail.com>
2022-03-14 14:50:49 +0900

    impl IntoArray for ndarray::ArrayN (#28)
    * simplify control flow in Graph::calculate
    
    * add comment
    
    * change return type
    
    * change return type
    
    * add IntoArray trait
    
    * make set_scalar_f32 unsafe
    
    * fix bug
    
    * impl IntoArray for ndarray::ArrayN
    
    * add comment
    
    * Update src/array/convert_ndarray.rs
    
    Co-authored-by: Tetsuo Kiso <tetsuok@users.noreply.github.com>
    
    Co-authored-by: Tetsuo Kiso <tetsuok@users.noreply.github.com>

--- src/array/convert_ndarray/tests.rs
+++ src/array/convert_ndarray/tests.rs
@@ -1,5 +1,6 @@
 use crate::array::*;
 use crate::hardware::cpu::CpuHardware;
+use ndarray::ShapeBuilder;
 
 #[test]
 fn test_try_into_ndarray0() {
@@ -189,4 +190,200 @@ fn test_try_into_ndarray6_fail() {
         let src = Array::fill_f32(&hw, Shape::from_slice(&vec![1; n]), 42.);
         assert!(ndarray::Array6::<f32>::try_from(&src).is_err());
     }
+}
+
+// Helper function to generate float range vector [0, end) with step size of 1.
+fn iota(end: i32) -> Vec<f32> {
+    (0..end).map(|x| x as f32).collect()
+}
+
+#[test]
+fn test_from_ndarray0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::arr0(123.);
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([]));
+    assert_eq!(dest.get_values_f32(), vec![123.]);
+}
+
+#[test]
+fn test_from_ndarray1() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::arr1(&iota(3));
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([3]));
+    assert_eq!(dest.get_values_f32(), iota(3));
+}
+
+#[test]
+fn test_from_ndarray1_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::arr1(&[]);
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([0]));
+    assert_eq!(dest.get_values_f32(), vec![]);
+}
+
+#[test]
+fn test_from_ndarray2() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array2::<f32>::from_shape_vec((2, 2), iota(4)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2]));
+    assert_eq!(dest.get_values_f32(), iota(4));
+}
+
+#[test]
+fn test_from_ndarray2_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array2::<f32>::from_shape_vec((0, 0), vec![]).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([0, 0]));
+    assert_eq!(dest.get_values_f32(), vec![]);
+}
+
+#[test]
+fn test_from_ndarray2_f() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array2::<f32>::from_shape_vec((2, 2).f(), iota(4)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2]));
+    assert_eq!(dest.get_values_f32(), vec![0., 2., 1., 3.]);
+}
+
+#[test]
+fn test_from_ndarray3() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array3::<f32>::from_shape_vec((2, 2, 2), iota(8)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2]));
+    assert_eq!(dest.get_values_f32(), iota(8));
+}
+
+#[test]
+fn test_from_ndarray3_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array3::<f32>::from_shape_vec((0, 0, 0), vec![]).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([0, 0, 0]));
+    assert_eq!(dest.get_values_f32(), vec![]);
+}
+
+#[test]
+fn test_from_ndarray3_f() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array3::<f32>::from_shape_vec((2, 2, 2).f(), iota(8)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2]));
+    assert_eq!(dest.get_values_f32(), vec![0., 4., 2., 6., 1., 5., 3., 7.]);
+}
+
+#[test]
+fn test_from_ndarray4() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array4::<f32>::from_shape_vec((2, 2, 2, 2), iota(16)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2, 2]));
+    assert_eq!(dest.get_values_f32(), iota(16));
+}
+
+#[test]
+fn test_from_ndarray4_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array4::<f32>::from_shape_vec((0, 0, 0, 0), vec![]).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([0, 0, 0, 0]));
+    assert_eq!(dest.get_values_f32(), vec![]);
+}
+
+#[test]
+fn test_from_ndarray4_f() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array4::<f32>::from_shape_vec((2, 2, 2, 2).f(), iota(16)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2, 2]));
+    #[rustfmt::skip]
+    assert_eq!(
+        dest.get_values_f32(),
+        vec![
+            0., 8., 4., 12., 2., 10., 6., 14.,
+            1., 9., 5., 13., 3., 11., 7., 15.,
+        ]
+    );
+}
+
+#[test]
+fn test_from_ndarray5() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array5::<f32>::from_shape_vec((2, 2, 2, 2, 2), iota(32)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2, 2, 2]));
+    assert_eq!(dest.get_values_f32(), iota(32));
+}
+
+#[test]
+fn test_from_ndarray5_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array5::<f32>::from_shape_vec((0, 0, 0, 0, 0), vec![]).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([0, 0, 0, 0, 0]));
+    assert_eq!(dest.get_values_f32(), vec![]);
+}
+
+#[test]
+fn test_from_ndarray5_f() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array5::<f32>::from_shape_vec((2, 2, 2, 2, 2).f(), iota(32)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2, 2, 2]));
+    #[rustfmt::skip]
+    assert_eq!(
+        dest.get_values_f32(),
+        vec![
+            0., 16.,  8., 24., 4., 20., 12., 28.,
+            2., 18., 10., 26., 6., 22., 14., 30.,
+            1., 17.,  9., 25., 5., 21., 13., 29.,
+            3., 19., 11., 27., 7., 23., 15., 31.,
+        ]
+    );
+}
+
+#[test]
+fn test_from_ndarray6() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array6::<f32>::from_shape_vec((2, 2, 2, 2, 2, 2), iota(64)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2, 2, 2, 2]));
+    assert_eq!(dest.get_values_f32(), iota(64));
+}
+
+#[test]
+fn test_from_ndarray6_0() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array6::<f32>::from_shape_vec((0, 0, 0, 0, 0, 0), vec![]).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([0, 0, 0, 0, 0, 0]));
+    assert_eq!(dest.get_values_f32(), vec![]);
+}
+
+#[test]
+fn test_from_ndarray6_f() {
+    let hw = RefCell::new(CpuHardware::new());
+    let src = ndarray::Array6::<f32>::from_shape_vec((2, 2, 2, 2, 2, 2).f(), iota(64)).unwrap();
+    let dest = src.into_array(&hw);
+    assert_eq!(*dest.shape(), Shape::new([2, 2, 2, 2, 2, 2]));
+    #[rustfmt::skip]
+    assert_eq!(
+        dest.get_values_f32(),
+        vec![
+            0., 32., 16., 48.,  8., 40., 24., 56.,
+            4., 36., 20., 52., 12., 44., 28., 60.,
+            2., 34., 18., 50., 10., 42., 26., 58.,
+            6., 38., 22., 54., 14., 46., 30., 62.,
+            1., 33., 17., 49.,  9., 41., 25., 57.,
+            5., 37., 21., 53., 13., 45., 29., 61.,
+            3., 35., 19., 51., 11., 43., 27., 59.,
+            7., 39., 23., 55., 15., 47., 31., 63.,
+        ]
+    );
 }
